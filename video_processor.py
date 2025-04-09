fd4569d6a3fe69fa12fea64079643e4bed050293
Yeabsira <yeabsiracherinet704@gmail.com>
2025-04-09 23:30:49 +0300

    add tiktok video downloader

--- video_processor.py
+++ video_processor.py
@@ -10,6 +10,7 @@ from random import randint
 from editor import Editor
 from graph_api import GraphApi
 
+
 class VideoProcessor:
     def __init__(self, user, payload, config) -> None:
         self.user = user
@@ -19,22 +20,28 @@ class VideoProcessor:
 
     def start_process(self):
         # Start a video job
-        query_db("INSERT INTO video_jobs (id, user_id, caption, config_id, created_at) VALUES (?, ?, ?, ?, ?);", self.job_id, self.user["id"], self.payload.get('title'), self.config['id'], datetime.datetime.now())
+        if self.payload.get("type") == "attachment":
+            query_db("INSERT INTO video_jobs (id, user_id, caption, config_id, created_at) VALUES (?, ?, ?, ?, ?);", self.job_id, self.user["id"], self.payload.get('title'), self.config['id'], datetime.datetime.now())
+            # Download video
+            video_path = self.download_attachment_video(self.payload["url"])
+            if not video_path:
+                self._update_status("Failed to download video", "failed")
+                return 
+        else:
+            query_db("INSERT INTO video_jobs (id, user_id, config_id, created_at) VALUES (?, ?, ?, ?);", self.job_id, self.user["id"], self.config['id'], datetime.datetime.now())
+            video_path = self.download_link_video(self.payload["url"])
+            if not video_path:
+                self._update_status("Failed to download video", "failed")
+                return 
         
-        video_path = self.download_video(self.payload["url"])
-        if not video_path:
-            self._update_status("Failed to download video", "failed")
-            return 
         # Update job status to download
         self._update_status(None, "downloaded")
-        
         if not self.config.get("split_type"):
             copy_path = Editor.copycat(video_path)
             if copy_path:
                 self._update_status(None, "completed")
             else:   
                 self._update_status("Couldn't copy video", "failed")
-                return 
         else:  
             gameplay_path = self.get_gameplay()
             if not gameplay_path:
@@ -42,28 +49,19 @@ class VideoProcessor:
                 return 
             
             self._update_status(None, "processing")
-            
-            edit_obj = Editor(video_path, gameplay_path, self.config)
+            try:
+                edit_obj = Editor(video_path, gameplay_path, self.config)
+            except Exception as es:
+                print(es)
+                self._update_status("error during processing", "failed")
+                return 
             video_url = edit_obj.start_editing(self.job_id)
             if video_url:
                 self._update_status(None, "completed")
             else:
                 self._update_status(None, "failed")
-                return 
-
-        # Post Automatically
-        print("Starting automatic post...")
-        title = self.payload.get('title')
-        accounts = self.get_social_accounts()
-        for acc in accounts:
-            query_db("INSERT INTO post_status (video_id, social_account_id, status) VALUES (?, ?, ?);", self.job_id, acc["id"], "posting")
-            
-            is_posted = self.postVideo(Config.reelsUrl + self.job_id + ".mp4", title, acc)   
-            status = "posted" if is_posted else "failed"
-            
-            self._update_post_status(status, acc["id"])   
     
-    def download_video(self, url):
+    def download_attachment_video(self, url):
         print(f"Download video with url: {url}")
         
         res = requests.get(url)    
@@ -77,7 +75,53 @@ class VideoProcessor:
                 file.write(chunk)
         
         return file_path        
+    
+    
+    def _tiktok_download(self, url):
+        res = requests.post("https://tikwm.com/api/", data={
+            "url": url,
+            "count": 12,
+            "cursor": 0,
+            "web": 1,
+            "hd": 1
+        })
+        if res.status_code != 200:
+            print(f"Error downloading video with url: {url}")
+            return
+        
+        data = res.json()
+        if not data.get("data"):
+            print(f"Couldn't get tiktok video data url: {url}")
+            return
+        
+        v_url = "https://tikwm.com"
+        
+        video = data["data"]
 
+        if video.get("play"):
+            v_url += video["play"]
+        else:
+            print(f"Couldn't get video to download for url: {url}")
+            return
+
+        res = requests.get(v_url)
+        
+        file_path = os.path.join(Config.reelsDirectory, self.job_id + "." + v_url.rsplit(".")[-1])
+        with open(file_path, "wb") as file:
+            for chunk in res.iter_content(100000):
+                file.write(chunk)
+        
+        query_db("UPDATE video_jobs SET caption = ? WHERE id = ?;", video.get("title"), self.job_id)
+
+        return file_path   
+        
+        
+    def download_link_video(self, url):
+        print(f"Download video with url: {url}")
+
+        if "tiktok" in self.payload["url"]:
+            return self._tiktok_download(url)
+            
     def get_gameplay(self):
         config_videos = query_db("SELECT gameplay_id FROM config_gameplays WHERE config_id = ?;", self.config["id"])
         if not config_videos:
@@ -90,35 +134,8 @@ class VideoProcessor:
         query_db("UPDATE video_jobs SET gameplay_id = ? WHERE id = ?;", gameplay_id, self.job_id)
         return os.path.join(Config.gameplayDirectory, config_videos[n]["gameplay_id"] + ".mp4")
     
-    def postVideo(self, video_url, title, acc):
-        graph_obj = GraphApi(access_token=acc["account_token"])
-        container = graph_obj.getReelContainer(video_url, caption=title)
-        if not container:
-            return 
-        
-        isUploaded = self.isUploadSuccessful(graph_obj, 0, container.get("id"))
-        if isUploaded:
-            return graph_obj.publishReelVideo(container.get("id"))
-        return None 
-    
-    def isUploadSuccessful(self, graph_obj, retry_count, id):
-        if retry_count > 3000:
-            return False
-        status = graph_obj.getMediaUploadStatus(id)
-        if status.get("status_code") != "FINISHED":
-            time.sleep(30)
-            return self.isUploadSuccessful(graph_obj, retry_count + 1, id)
-        return True
-    
-    def get_social_accounts(self):
-        accounts = query_db("SELECT * FROM video_config_account cg JOIN ConnectedAccounts ca ON cg.social_account_id = ca.id WHERE cg.config_id = ?;", self.config["id"])        
-        return accounts or []
-    
     def _update_status(self, message, status):
         if message:
             query_db("UPDATE video_jobs SET processing_errors = ?, status = ? WHERE id = ?;", message, status, self.job_id)
         else:
             query_db("UPDATE video_jobs SET status = ? WHERE id = ?;", status, self.job_id)
-
-    def _update_post_status(self, status, account_id):
-        query_db("UPDATE post_status SET status = ? WHERE video_id = ? AND social_account_id = ?;", status, self.job_id, account_id)

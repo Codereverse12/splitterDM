df4f085c64f80469d1cc71be228d3fa4d0deadf3
Yeabsira <yeabsiracherinet704@gmail.com>
2025-04-15 09:28:11 +0300

    transform sqlite3 schema to postgreql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT,
-    register_date NUMERIC NOT NULL,
+    register_date TIMESTAMP NOT NULL,
     ig_id INTEGER UNIQUE,
     ig_username TEXT UNIQUE,
     default_config_id TEXT
@@ -25,8 +25,8 @@ CREATE TABLE IF NOT EXISTS video_configurations (
     video_position TEXT CHECK (video_position IN ('left', 'right', 'top', 'bottom')),
     edit_type TEXT CHECK (edit_type IN ('crop', 'fit')),
     original_video_percentage INTEGER CHECK(original_video_percentage BETWEEN 0 AND 100),
-    created_at NUMERIC NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
+    created_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS config_gameplays (
@@ -47,9 +47,8 @@ CREATE TABLE IF NOT EXISTS video_jobs (
     video_type TEXT CHECK (video_type IN ('tiktok', 'youtube', 'instagram')),
     status TEXT CHECK (status IN ('pending', 'downloaded', 'processing', 'completed', 'failed')) DEFAULT 'pending',
     processing_errors TEXT,
-    created_at NUMERIC NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (config_id) REFERENCES video_configurations(id) ON DELETE SET NULL,
     FOREIGN KEY (gameplay_id) REFERENCES gameplays(id) ON DELETE SET NULL
 );
-

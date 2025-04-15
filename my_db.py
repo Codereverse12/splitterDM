7b9f16fb9c304dbe54aaa314dab8e6924086ca60
Yeabsira <yeabsiracherinet704@gmail.com>
2025-04-15 09:51:46 +0300

    change database connection to postgresql instead of sqlite3

--- my_db.py
+++ my_db.py
@@ -1,16 +1,17 @@
 from cs50 import SQL
 import logging
-from flask import abort, redirect, flash
+from config import Config
+from urllib.parse import quote
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///autoSplitter.db")
+db = SQL(f"postgresql://{Config.dbUser}:{quote(Config.dbPass)}@{Config.dbHost}:{Config.dbPort}/{Config.dbName}")
 
 def query_db(*args):
     try:
         return db.execute(*args)
     except Exception as e:
         logging.error(f"Database error: {e}")
-        abort(500, "Server error try again")
+        raise
